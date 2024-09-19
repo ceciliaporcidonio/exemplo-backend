941eb31285af2f43fce2f3ff980e58c9c0e8e66e
Ana Cecilia Porcidonio <cecilia.pordidonio@gmail.com>
2024-09-19 01:17:48 -0300

    first commit

---
+++ PrimeiroProjetoJava/src/br/com/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args [0];
+		
+		if (ambiente.equals("DEV"))
+		System.out.print("DEV");
+	}
+
+}
---
+++ PrimeiroProjetoJava/src/br/com/Teste1.java
@@ -0,0 +1,8 @@
+package br.com;
+
+public class Teste1 {
+	
+	public static void main (String [] args) {
+	
+	}
+}
